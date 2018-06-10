6722f94b51bb51eef58ad20f2b95ccf432ec51e7
Gornostalev <30836610+Gornostalev@users.noreply.github.com>
2018-06-11 00:30:03 +0500

    //

--- CompileNotepad++.ps1
+++ CompileNotepad++.ps1
@@ -1,9 +1,7 @@
-﻿$NameCS = Get-ChildItem . -Include *.cs -Recurse | # переменной $NameCS присваивается путь к последнему изменённому файлу .cs
+﻿$NameCS = (Get-ChildItem . -Include *.cs -Recurse | Sort-Object LastWriteTime -Descending | Select-Object Name -First 1 -exp fullname)  # переменной $NameCS присваивается путь к последнему изменённому файлу .cs
 
-Sort-Object LastWriteTime -Descending | Select-Object Name -First 1 -exp fullname 
-
-C:\Windows\Microsoft.NET\Framework64\v3.5\csc.exe $NameCS  # компиляция
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe $NameCS  # компиляция
 
 $ProgramExecute = Get-ChildItem -Include *.exe -Recurse | Sort-Object LastWriteTime -Descending | Select-Object Name -First 1 -exp fullname     # переменная $ProgramExecute получает путь к недавно созданному .exe файлу 
  
-Invoke-Item $ProgramExecute # запуск последнейскомпилированной программы 
+Invoke-Item $ProgramExecute # запуск последней скомпилированной программы 
